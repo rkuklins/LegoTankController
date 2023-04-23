0fd8de37378a57944989458c2c2fa73f2b268bb1
Rafal <rav.kuklinski@gmail.com>
2023-04-23 18:07:41 +0200

    Initial Commit

---
+++ LegoTank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LegoTank
+//
+//  Created by Rafal Kuklinski on 23/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
